6e4d778a52175c58b94719bd12b318e2f8b0446e
rudreshkumar25 <40913419+rudreshkumar25@users.noreply.github.com>
2018-07-07 16:44:11 +0530

    3rd code change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>Hello rudresh kumar Welcome to World of AWS</h1>
+<h1>Hello rudresh kumar Welcome to World of AWS Third change rejecting QA</h1>
 
 <?php
 echo "Version 2.1.2";
